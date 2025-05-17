b74e7e896d233df0cb7ddb546f814e18dc32d2ad
vaishnavi <vishu032004@gmail.com>
2025-05-17 19:49:32 +0530

    half then half

--- project_name/lib/User_Page/Usermain.dart
+++ project_name/lib/User_Page/Usermain.dart
@@ -11,8 +11,21 @@ class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0x2B2E37),
       appBar: AppBar(
-        title: Text("Good Morning"),
+          backgroundColor: Color(0x2B2E37),
+        title: Text("Good Morning",
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w200,
+        color: Color(0xFF686F82),
+
+        ),
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.person_pin))
+        ],
+        
       ),
     );
   }
