47bdd4f25630cc9ad2ed49c1adb5b9b808f989ed
Sahil4430 <panwarsahil204@gmail.com>
2025-06-17 14:09:24 +0530

    positioned set

--- project_name/lib/Recipe_page/Recipe_page.dart
+++ project_name/lib/Recipe_page/Recipe_page.dart
@@ -7,12 +7,18 @@ class RecipePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold
-    (
+    final Width = MediaQuery.of(context).size.width;
+    return Scaffold(
       body: Stack(
-        children: [ Recipe_bg(),
-        Content(),
-          
+        children: [
+          Recipe_bg(),
+          Positioned(
+            top: -(Width/11), // Negative value moves it upward, adjust as needed
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Content(),
+          ),
         ],
       ),
     );
